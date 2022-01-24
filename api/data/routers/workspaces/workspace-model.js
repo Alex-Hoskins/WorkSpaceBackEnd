d0a288d759e8de2dfa925a1d1449b1a862f5d63f
Alex Hoskins <alexander.hoskins@yahoo.com>
2022-01-24 11:45:35 -0800

    post a workspace started

--- api/data/routers/workspaces/workspace-model.js
+++ api/data/routers/workspaces/workspace-model.js
@@ -6,6 +6,18 @@ async function getAllWorkSpaces() {
     return rows 
   }
 
+async function addWorkSpace(item, image) {
+    const [newItem] = await db('workspaces').insert(item,['workspace_id', 'workspace_name','workspace_description', 'workspace_location', 'workspace_price'])
+    image.workspace_id= newItem.workspace_id
+
+    if(image.image_content){
+      const [newImage] = await db('images').insert(image,['image_content', 'image_description'])
+      newItem.image = newImage
+    }
+    return newItem 
+  }
+
 module.exports={
     getAllWorkSpaces,
+    addWorkSpace,
 }
--- api/data/routers/workspaces/workspace-router.js
+++ api/data/routers/workspaces/workspace-router.js
@@ -9,13 +9,23 @@ router.get('/', (req, res, next)=>{
     .catch(next)
 })
 
-// router.post('/', (req, res, next)=>{
-//     Workspace.insertWorkspace(req.body)
-//     .then(workspace=>{
-//         res.status(200).json(workspace)
-//     })
-//     .catch(next)
-// })
+router.post('/', (req, res, next)=>{
+    let item ={ 
+        workspace_name: req.body.workspace_name,
+        workspace_description:  req.body.workspace_description,
+        workspace_location: req.body.workspace_location,
+        workspace_price: req.body.workspace_price
+    }
+    let image = {
+        image_content: req.body.image_content,
+        image_description: req.body.image_description || ''
+    }
+    Workspace.addWorkSpace(item, image)
+    .then(workspace=>{
+        res.status(200).json(workspace)
+    })
+    .catch(next)
+})
 
 router.use((err, req, res, next)=>{
     res.status(500).json({
